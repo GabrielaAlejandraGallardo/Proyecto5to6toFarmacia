ab07d57a35c38a095476b4af7dcf6490b302426d
GabrielaAlejandraGallardo <87786204+GabrielaAlejandraGallardo@users.noreply.github.com>
2025-10-13 11:22:04 -0300

    Update urls.py

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -1,16 +1,24 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for myworld project.
 
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add/', views.add, name='add'),
-    path('add/addrecord/', views.addrecord, name='addrecord'),
-    path('delete/<int:id>', views.delete, name='delete'),
-    path('update/<int:id>', views.update, name='update'),
-    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+    path('', include('medicamentos.urls')),
+    path('medicamentos/', include('medicamentos.urls')),
+    path('admin/', admin.site.urls),
 
-    ]
-
-
-
+]
